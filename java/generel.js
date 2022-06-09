48777467be5b2cf13559e136a1058acc460ead5a
steffen kroyer <stef5450@stud.kea.dk>
2022-06-09 19:11:10 +0200

    this ones a big one

--- java/generel.js
+++ java/generel.js
@@ -2,6 +2,11 @@ const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 
 burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navLinks.classList.toggle("active");
-})
+  burger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
